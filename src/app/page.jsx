c2413244d810052675e8a871dcf183f91efc5592
Your Name <you@example.com>
2025-04-18 20:59:21 +0700

    first

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>NextJS CRUD + MongoDB</h1>
+      <hr className="my-3" />
+      <div></div>
+    </main>
+  );
+}
